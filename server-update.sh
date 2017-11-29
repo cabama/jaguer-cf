1432f00b487fbda6885d2dc48a19e2434e790950
Carlos Barreiro Mata <barreymata@gmail.com>
2017-11-29 03:53:15 +0100

    temporal: automatic pull

--- server-update.sh
+++ server-update.sh
@@ -1,5 +1,6 @@
-git pull
-cd ng-jaguer-madrid && git pull
-cd ../node-jaguer-madrid && git pull
+. ../gitgit.sh
+git pull https://$gitus:$gitpp@github.com/cabama/jaguer-cf.git
+cd ng-jaguer-madrid && git pull https://$gitus:$gitpp@github.com/cabama/ng-jaguer-madrid.git
+cd ../node-jaguer-madrid && git pull https://$gitus:$gitpp@github.com/cabama/node-jaguer-madrid.git
 cd ..
 docker-compose up
